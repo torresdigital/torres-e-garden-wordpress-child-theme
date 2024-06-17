27164daca4ae8bb59c714f1e5fc5963b4271a7cc
Anderson Torres <atorresnh@gmail.com>
2024-06-17 01:59:03 -0300

    Update menu-mobile.php

--- torres-e-garden/template-parts/menu/menu-mobile.php
+++ torres-e-garden/template-parts/menu/menu-mobile.php
@@ -7,6 +7,11 @@
 
 ?>
 <nav id="mobile-navigation" class="cenote-mobile-navigation">
+
+                <nav class="main-navigation cenote-sticky-navigation tg-site-menu--default">
+                        <!-- Torres Digital Logo Sticky -->
+                    <a id="torres-digital-logo-sticky" class="torres-digital-logo-sticky" href="/"></a>
+				</nav>
 	<?php
 	wp_nav_menu( array(
 		'theme_location' => 'tg-menu-primary',
